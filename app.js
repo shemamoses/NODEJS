3dbb14d41fbf4bd019b33ac26c450fdd8ef8295b
shemamoses <mosesshema1@gmail.com>
2023-06-24 17:03:03 +0200

    .

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
 
-const server = http.createServer(app);
+app.use((req, res, next) => {
+  console.log("this is a middleware ");
+  next();
+});
+app.use((req, res, next) => {
+  console.log("this is another middleware ");
+  res.send("hello from express");
+});
 
-server.listen(3000);
+app.listen(3000);
