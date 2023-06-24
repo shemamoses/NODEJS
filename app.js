ec1c862fcf6f138a6184d5366414866cea53f34e
shemamoses <mosesshema1@gmail.com>
2023-06-24 17:17:21 +0200

    .

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("this is another middleware ");
-  res.send("hello from express");
+app.use("/add-product", (req, res, next) => {
+  res.send("add product page");
+});
+app.use("/", (req, res, next) => {
+  res.send("home page");
 });
 
 app.listen(3000);
