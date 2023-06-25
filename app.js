43f5df3839dedb2e0e56b80a2d6c96951b1c7e21
shemamoses <mosesshema1@gmail.com>
2023-06-25 11:59:47 +0200

    .

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require("express");
 
 const app = express();
 
-app.use("/add-product", (req, res, next) => {
+app.use("/product", (req, res, next) => {
   res.send("add product page");
 });
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">ADD PRODUCT</button></form>'
+  );
+});
+
 app.use("/", (req, res, next) => {
   res.send("home page");
 });
