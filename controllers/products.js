ab6819abd873994ca42fb6e36681ad5b67cd6ccd
shemamoses <mosesshema1@gmail.com>
2023-07-04 22:35:44 +0200

    .

--- controllers/products.js
+++ controllers/products.js
@@ -10,19 +10,19 @@ exports.getAddProduct = (req, res, next) => {
   });
 };
 
-// const postAddProducts = (req, res, next) => {
-//   const products = adminData.products;
-//   res.render("shop", {
-//     prods: products,
-//     pageTitle: "Shop",
-//     path: "/",
-//     hasProducts: products.length > 0,
-//     activeShop: true,
-//     productCSS: true,
-//   });
-// };
-
 const postAddProducts = (req, res, next) => {
   products.push({ title: req.body.title });
   res.redirect("/");
+};
+
+const getProducts = (req, res, next) => {
+  const products = adminData.products;
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
 };
