848ba4ce47eeb7cd46b03b6ea5e8a3e89406da56
shemamoses <mosesshema1@gmail.com>
2023-07-04 22:19:39 +0200

    .

--- routes/shop.js
+++ routes/shop.js
@@ -7,16 +7,6 @@ const adminData = require('./admin');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-  });
-});
+router.get('/', );
 
 module.exports = router;
