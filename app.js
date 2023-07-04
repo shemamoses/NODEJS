7db38deb3d202e6d67d7d2ca950923e566bcc4ac
shemamoses <mosesshema1@gmail.com>
2023-07-04 21:46:45 +0200

    .

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use("/", (req, res, next) => {
-  res.send("home page");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
